2d8c0daf76764b0e946023c739e27c58b2992254
João Hudson <joao.hudson@taking.com.br>
2023-04-25 23:32:22 -0300

    refatorando main

--- static/main.js
+++ static/main.js
@@ -1,30 +1,31 @@
 import { SetupComponent } from "/components/setup.js"
 import { ChatComponent } from "/components/chat.js";
 
-(async function(){
-    
 const [h1] = document.getElementsByTagName('h1');
 
-const dictionaryResponse = await fetch('/api/lang');
-if(!dictionaryResponse.ok){
-    alert(await dictionaryResponse.text());
-    return;
+async function main(){
+    
+    const dictionaryResponse = await fetch('/api/lang');
+    if(!dictionaryResponse.ok){
+        alert(await dictionaryResponse.text());
+        return;
+    }
+    const dictionary = await dictionaryResponse.json();
+
+    const chatComponent = new ChatComponent({
+        div: document.getElementById('chatDiv'),
+        h1: h1,
+        dictionary: dictionary
+    });
+
+    const setupComponent = new SetupComponent({
+        div: document.getElementById('setupDiv'),
+        h1: h1,
+        dictionary: dictionary,
+        nextPage: chatComponent
+    });
+
+    setupComponent.show();
 }
-const dictionary = await dictionaryResponse.json();
 
-const chatComponent = new ChatComponent({
-    div: document.getElementById('chatDiv'),
-    h1: h1,
-    dictionary: dictionary
-});
-
-const setupComponent = new SetupComponent({
-    div: document.getElementById('setupDiv'),
-    h1: h1,
-    dictionary: dictionary,
-    nextPage: chatComponent
-});
-
-setupComponent.show();
-
-}());
+main();

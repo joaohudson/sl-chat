ad80c079420c12ac931a339f376bdb0cd4618770
João Hudson <joao.hudson@taking.com.br>
2023-05-07 17:32:35 -0300

    adicionando tempo no painel de gravação de áudio

--- static/panels/audio-recorder.js
+++ static/panels/audio-recorder.js
@@ -1,5 +1,11 @@
 import { AudioRecorder } from '/record/audio-recorder.js';
 
+function formatTime(nanoSeconds){
+    const seconds = Math.floor(nanoSeconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    return (minutes % 60).toString().padStart(2, '0') + ' : ' + (seconds % 60).toString().padStart(2, '0');
+}
+
 class AudioRecorderPanel{
     constructor({div, dictionary}){
         this.div = div;
@@ -8,6 +14,7 @@ class AudioRecorderPanel{
         this.displayAudioRecorder = div.querySelector('#displayAudioRecorder');
         this.audioRecorderSendButton = div.querySelector('#audioRecorderSendButton');
         this.audioRecorderCancelButton = div.querySelector('#audioRecorderCancelButton');
+        this.timeAudioRecorder = div.querySelector('#timeAudioRecorder');
     }
 
     async show(){
@@ -17,21 +24,44 @@ class AudioRecorderPanel{
         this.audioRecorderSendButton.innerText = this.dictionary.Send;
         this.audioRecorderCancelButton.innerText = this.dictionary.Cancel;
 
+        this.#resetTime();
+
         const promise = new Promise((res) => {
             this.audioRecorderSendButton.onclick = async () => {
-                this.#hide();
+                this.#onStop();
                 res(await this.audioRecorder.complete());
             };
             this.audioRecorderCancelButton.onclick = () => {
                 this.audioRecorder.cancel();
-                this.#hide();
+                this.#onStop();
                 res(null);
             };
         });
 
         await this.audioRecorder.record();
+        this.#onStart();
 
         return promise;
+    }
+
+    #onStart(){
+        this.#runTime();
+    }
+
+    #onStop(){
+        clearInterval(this.timeInterval);
+        this.#hide();
+        this.#resetTime();
+    }
+
+    #resetTime(){
+        this.timeAudioRecorder.innerText = '00 : 00';
+    }
+
+    #runTime(){
+        this.timeInterval = setInterval(() => {
+            this.timeAudioRecorder.innerText = formatTime(this.audioRecorder.getTime());
+        }, 1000);
     }
 
     #hide(){

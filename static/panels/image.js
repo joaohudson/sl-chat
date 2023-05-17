bc21f1a43bad49ef9e914f7aa3a3d0544e86708c
João Hudson <joao.hudson@taking.com.br>
2023-05-17 19:19:18 -0300

    fix image size in fullscreen panel

--- static/panels/image.js
+++ static/panels/image.js
@@ -8,6 +8,17 @@ class ImagePanel{
         this.div.style.display = '';
         this.image.src = url;
         this.image.onclick = () => this.#hide();
+        this.image.onload = () => this.#fixImageSize();
+    }
+
+    #fixImageSize(){
+        if(this.image.width > window.screen.width){
+            this.image.style.width = '100%';
+            this.image.style.height = 'auto';
+        }else{
+            this.image.style.width = 'auto';
+            this.image.style.height = '100%';
+        }
     }
 
     #hide(){

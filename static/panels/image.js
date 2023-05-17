00cb9ab573dc5b7115931e30ccd9bc63b846b3d8
João Hudson <joao.hudson@taking.com.br>
2023-05-17 18:50:52 -0300

    corrigindo exibição de imagem fullscreen

--- static/panels/image.js
+++ static/panels/image.js
@@ -8,13 +8,6 @@ class ImagePanel{
         this.div.style.display = '';
         this.image.src = url;
         this.image.onclick = () => this.#hide();
-        if(this.image.width > this.image.height){
-            this.image.style.width = '100%';
-            this.image.style.height = 'auto';
-        }else{
-            this.image.style.width = 'auto';
-            this.image.style.height = '100%';
-        }
     }
 
     #hide(){

806b07d786e67504c85f62300ef6690a627e380b
João Hudson <joao.hudson@taking.com.br>
2023-05-17 00:42:34 -0300

    completando erros

--- static/error/errors-table.js
+++ static/error/errors-table.js
@@ -1,7 +1,7 @@
 const errorToTextKey = {
     SLERR_ROOM_NOT_FOUND: "roomNotFoundError",
-    SLERR_ROOM_MAX_USER: "roomMaxUserError", //reserved
-    SLERR_SERVER_MAX_USER: "serverMaxUserError", //reserved
+    SLERR_ROOM_MAX_USER: "roomMaxUserError",
+    SLERR_SERVER_MAX_USER: "serverMaxUserError",
     LOCAL_ERROR_ILLEGAL_STATE: "unespectedError",
     LOCAL_ERROR_MICROPHONE_ACCESS: "microphoneAcessError"
 };
